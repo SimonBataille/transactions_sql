dfc755040761c1b690775885d26d333085ad60f1
Simon Bataille <bataille.simon119@gmail.com>
2022-08-17 12:24:50 -1000

    Update script to create db

--- myscript.sql
+++ myscript.sql
@@ -1,4 +1,21 @@
-CREATE TABLE account_statement_no_header(
+CREATE TABLE swb(
+      swbID INTEGER PRIMARY KEY AUTOINCREMENT,
+      "Local time" TEXT,
+      "Time in UTC" TEXT,
+      "Type" TEXT,
+      "Currency" TEXT,
+      "Gross amount" REAL,
+      "Gross amount (EUR)" REAL,
+      "Fee" REAL,
+      "Fee (EUR)" REAL,
+      "Net amount" REAL,
+      "Net amount (EUR)" REAL,
+      "Note" TEXT,
+      mtpID INTEGER,
+      FOREIGN KEY (mtpID)
+           REFERENCES mtp (mtpID)
+);
+CREATE TABLE tmp_swb(
       "Local time" TEXT,
       "Time in UTC" TEXT,
       "Type" TEXT,
@@ -10,4 +27,23 @@ CREATE TABLE account_statement_no_header(
       "Net amount" REAL,
       "Net amount (EUR)" REAL,
       "Note" TEXT
+);
+CREATE TABLE mtp(
+      mtpID INTEGER PRIMARY KEY AUTOINCREMENT,
+      "Date" TEXT,
+      "Origin" TEXT,
+      "Currency In" TEXT,
+      "In" REAL,
+      "Destination" TEXT,
+      "Currency Out" TEXT,
+      "Out" REAL
+);
+CREATE TABLE tmp_mtp(
+      "Date" TEXT,
+      "Origin" TEXT,
+      "Currency In" TEXT,
+      "In" REAL,
+      "Destination" TEXT,
+      "Currency Out" TEXT,
+      "Out" REAL
 );
